b0f52de1a55827c5c758b1f0a55bb59f29e80013
Quexint <Quexint@gmail.com>
2015-11-11 22:50:55 +0800

    Add Python into PATH

--- env.sh
+++ env.sh
@@ -2,6 +2,6 @@
 
 # PATH
 alias cProf='instruments -t "/Applications/Xcode.app/Contents/Applications/Instruments.app/Contents/Resources/templates/Time Profiler.tracetemplate" -D res.log '
-export PATH="/Developer/NVIDIA/CUDA-5.5/bin:/usr/local/sbin:~/.cabal/bin:/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin:/opt/X11/bin:/usr/texbin"
+export PATH="/usr/local/share/python:/Developer/NVIDIA/CUDA-5.5/bin:/usr/local/sbin:~/.cabal/bin:/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin:/opt/X11/bin:/usr/texbin"
 
 function newcpp () { cp ~/sample.cpp ./"$@".cpp }
