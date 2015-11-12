1eb438110f604ebdf40f0c4bf82ad5f31d742abf
Quexint <Quexint@gmail.com>
2015-11-12 14:25:57 +0800

    Add 'JAVA_HOME' into environment variables

--- env.sh
+++ env.sh
@@ -3,5 +3,5 @@
 # PATH
 alias cProf='instruments -t "/Applications/Xcode.app/Contents/Applications/Instruments.app/Contents/Resources/templates/Time Profiler.tracetemplate" -D res.log '
 export PATH="/usr/local/share/python:/Developer/NVIDIA/CUDA-5.5/bin:/usr/local/sbin:~/.cabal/bin:/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin:/opt/X11/bin:/usr/texbin"
-
+export JAVA_HOME="`/usr/libexec/java_home -v 1.8`"
 function newcpp () { cp ~/sample.cpp ./"$@".cpp }
