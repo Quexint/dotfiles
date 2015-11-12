98fdcf1ea2f70305630bb0b1aa835366351d1854
Quexint <Quexint@gmail.com>
2015-11-12 14:29:59 +0800

    Add Ruby Environment Variables

--- env.sh
+++ env.sh
@@ -4,4 +4,5 @@
 alias cProf='instruments -t "/Applications/Xcode.app/Contents/Applications/Instruments.app/Contents/Resources/templates/Time Profiler.tracetemplate" -D res.log '
 export PATH="/usr/local/share/python:/Developer/NVIDIA/CUDA-5.5/bin:/usr/local/sbin:~/.cabal/bin:/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin:/opt/X11/bin:/usr/texbin"
 export JAVA_HOME="`/usr/libexec/java_home -v 1.8`"
+eval "$(rbenv init -)"
 function newcpp () { cp ~/sample.cpp ./"$@".cpp }
