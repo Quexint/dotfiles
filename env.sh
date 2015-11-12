fb0a77d13df830ded047adee4811dc9f4e477d15
Quexint <Quexint@gmail.com>
2015-11-12 14:51:48 +0800

    Add hub environment varible into env.sh

--- env.sh
+++ env.sh
@@ -5,4 +5,5 @@ alias cProf='instruments -t "/Applications/Xcode.app/Contents/Applications/Instr
 export PATH="/usr/local/share/python:/Developer/NVIDIA/CUDA-5.5/bin:/usr/local/sbin:~/.cabal/bin:/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin:/opt/X11/bin:/usr/texbin"
 export JAVA_HOME="`/usr/libexec/java_home -v 1.8`"
 eval "$(rbenv init -)"
+eval "$(hub alias -s)"
 function newcpp () { cp ~/sample.cpp ./"$@".cpp }
