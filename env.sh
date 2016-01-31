df26220f7937c4d912982584024c1fec1543433e
Quexint <Quexint@gmail.com>
2016-01-31 18:02:14 +0800

    Add SD Card into aliase

--- env.sh
+++ env.sh
@@ -1,10 +1,12 @@
 #!/bin/zsh
 
 # PATH
-export PATH="$(brew --prefix coreutils)/libexec/gnubin:/usr/local/share/python:/Developer/NVIDIA/CUDA-5.5/bin:/usr/local/sbin:~/.cabal/bin:/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin:/opt/X11/bin:/usr/texbin"
+eval "$(hub alias -s)"
+pushd $(brew --prefix root) >/dev/null; . libexec/thisroot.sh; popd >/dev/null
+export PATH="$(brew --prefix coreutils)/libexec/gnubin:/usr/local/share/python:/usr/local/sbin:~/.cabal/bin:/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin:/opt/X11/bin:/usr/texbin:$PATH:/usr/local/cuda/bin"
+export DYLD_LIBRARY_PATH="/usr/local/cuda/lib:$DYLD_LIBRARY_PATH"
 export JAVA_HOME="`/usr/libexec/java_home -v 1.8`"
 if which rbenv > /dev/null; then eval "$(rbenv init -)"; fi
-eval "$(hub alias -s)"
 
 # Function
 function mkd() { mkdir -p "$@" && cd "$_"; }
