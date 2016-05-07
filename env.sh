353cf9cc92ab6477bb4b86c0252755583446d280
Quexint <Quexint@gmail.com>
2016-05-07 17:02:25 +0800

    Add Rbenv Path

--- env.sh
+++ env.sh
@@ -6,12 +6,12 @@ then
   pushd $(brew --prefix root) >/dev/null; . libexec/thisroot.sh; popd >/dev/null
   export PATH="$(brew --prefix coreutils)/libexec/gnubin:/usr/local/sbin:~/.cabal/bin:/usr/local/bin:~/anaconda3/bin:/usr/bin:/bin:/usr/sbin:/sbin:/opt/X11/bin:/usr/texbin:$PATH:/usr/local/cuda/bin"
   export DYLD_LIBRARY_PATH="/usr/local/cuda/lib:$DYLD_LIBRARY_PATH"
-  export DYLD_LIBRARY_PATH="/usr/local/Cellar/boost/1.60.0_2/lib:$DYLD_LIBRARY_PATH"
   export JAVA_HOME="`/usr/libexec/java_home -v 1.8`"
   set RANGER_LOAD_DEFAULT_RC=FALSE
 else
 fi
 
+export PATH="$HOME/.rbenv/bin:$PATH"
 # Ruby PATH
 if which rbenv > /dev/null;
 then
