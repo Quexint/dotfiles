6c7c2f21c23221e5ca84df9c498c48b9cce842f9
MingChia Chung <Quexint@gmail.com>
2018-08-31 11:41:36 +0800

    feat: Add bat integration

--- zsh/custom/integration.zsh
+++ zsh/custom/integration.zsh
@@ -2,6 +2,9 @@
 [[ -e $(which brew 2>/dev/null) ]] && [[ -s $(brew --prefix)/etc/profile.d/autojump.sh ]] && . $(brew --prefix)/etc/profile.d/autojump.sh
 [[ -e /usr/share/autojump/autojump.sh ]] && . /usr/share/autojump/autojump.sh
 
+#======================== bat ========================
+[[ -e $(which bat 2>/dev/null) ]] && alias cat='bat'
+
 #======================== hub ========================
 if [[ -e /usr/local/bin/hub ]]; then
   eval "$(hub alias -s)"
@@ -18,5 +21,6 @@ fi
 
 #======================== fuzzy finder  ========================
 export FZF_DEFAULT_OPTS="--reverse --inline-info --preview 'head -100 {}'"
+[[ -e $(which bat 2>/dev/null) ]] && export FZF_DEFAULT_OPTS="--reverse --inline-info --preview 'bat --color "always" {}'"
 [ -f ~/.fzf.zsh ] && source ~/.fzf.zsh
 
