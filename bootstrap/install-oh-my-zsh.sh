7ccfb857b06242534b5887e5158ba64dddb5dc7b
MingChia Chung <Quexint@gmail.com>
2018-12-03 14:56:04 +0800

    feat: ZSH: Use wget to fetch oh-my-zsh

--- bootstrap/install-oh-my-zsh.sh
+++ bootstrap/install-oh-my-zsh.sh
@@ -2,5 +2,5 @@
 
 set -euxo pipefail
 
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
 rm $HOME/.zshrc
