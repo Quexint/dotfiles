39a3a8db81084e58c2bfef35881a866ebadca15f
MingChia Chung <Quexint@gmail.com>
2020-01-05 16:05:43 +0800

    fix: vim bootstrap script

--- bootstrap/install-vim.sh
+++ bootstrap/install-vim.sh
@@ -2,6 +2,7 @@
 
 set -euxo pipefail
 
+mkdir -p $HOME/.vim/bundle
 git clone https://github.com/VundleVim/Vundle.vim.git $HOME/.vim/bundle/Vundle.vim
 vim +PluginInstall +qall
 cd $HOME/.vim/bundle/YouCompleteMe && ./install.py --clang-completer
