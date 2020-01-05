283e9aa09e2fc43da3a5f9970dbe222274bf4b20
MingChia Chung <Quexint@gmail.com>
2020-01-05 23:03:50 +0800

    feat: Remove unused scripts

--- hacker_scripts/docker-cleanup.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-# Remove all containers
-docker rm $(docker ps -a -q)
-# Remove all images
-docker rmi $(docker images -q)
--- hacker_scripts/updateSubRepo.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-pushd "$(pwd)"
-for d in ./*/ ; do
-  if [ -d "$d/.git" ]; then
-    echo "Updating \"$d\"..."
-    (cd "$d" && git fetch && git pull);
-  fi
-done
